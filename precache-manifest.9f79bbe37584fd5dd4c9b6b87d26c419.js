7943d6cd9bdc5240eec65de04659daebe4a52ef4
Mark Bennett <bennettwmark@gmail.com>
2020-04-25 17:16:14 -0700

    Updates

---
+++ precache-manifest.9f79bbe37584fd5dd4c9b6b87d26c419.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17c6ffa185c48a358e46e60b568a30d",
+    "url": "/35/index.html"
+  },
+  {
+    "revision": "8706b95c7f0b4a3801eb",
+    "url": "/35/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a4a746614ff7ebcc96c8",
+    "url": "/35/static/js/2.53e13871.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/35/static/js/2.53e13871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8706b95c7f0b4a3801eb",
+    "url": "/35/static/js/main.fbad54e1.chunk.js"
+  },
+  {
+    "revision": "ccb6ac92d5566f653a4d",
+    "url": "/35/static/js/runtime-main.3e285a96.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/35/static/media/logo.5d5d9eef.svg"
+  }
+]);
