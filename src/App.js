f3a49a03df31b4a045f4a6da356f1f30fe1ed2f0
Mark Bennett <bennettwmark@gmail.com>
2020-04-25 18:23:45 -0700

    Adds AOS for scrolling animations

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
+import AOS from 'aos';
 
 import Title from './Title.js';
 import Text from './Text.js';
 import { contentSections } from './content.js';
 
 import './App.css';
+import 'aos/dist/aos.css';
 
-const ContentSection = ({ section }) => {
-  switch (section.type) {
-    case "text":
-      return (<Text>{section.text}</Text>)
-    default:
-      return null
-  }
-}
+// Initialize AOS
+AOS.init();
+
+const ContentSection = ({ section }) => (
+  <div className="ContentSection">
+    {section.type === "text" &&
+      (<Text>{section.text}</Text>)
+    }
+  </div>
+)
 
 function App() {
   return (
     <div className="App">
       <Title title="35" subtitle="👇Scroll down 👇" />
       {contentSections.map((section, idx) => (
-        <div key={idx}>
+        <div key={idx} data-aos={section.aos}>
           <ContentSection section={section} />
         </div>
       ))}
